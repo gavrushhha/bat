b95bd978e191ab88794b4b3fb1212d60a07d68d5
gavrushhha <91607838+gavrushhha@users.noreply.github.com>
2026-03-12 16:49:18 +0300

    Update RunRobocopyVersioned.bat

--- RunRobocopyVersioned.bat
+++ RunRobocopyVersioned.bat
@@ -1,5 +1,1 @@
-@echo off
-REM Запуск Robocopy в режиме версий (имя_файла_дата_время.ext) с config\Config.ini
-cd /d "%~dp0robocopy"
-call RobocopyMonitor.bat "%~dp0config\Config.ini" /VERSIONED
-pause
+
